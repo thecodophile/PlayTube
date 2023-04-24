cee45658d2882d03082d2a28a1ab8080d0f97513
Somnath Dey <sdey.aklakhi@gmail.com>
2023-04-24 10:22:28 +0530

    update some icon of leftnav

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,13 +4,13 @@ import { AiFillHome, AiOutlineFlag } from "react-icons/ai";
 import { MdLocalFireDepartment, MdLiveTv } from "react-icons/md";
 import { CgMusicNote } from "react-icons/cg";
 import { FiFilm } from "react-icons/fi";
-import { IoGameControllerSharp } from "react-icons/io5";
 import { ImNewspaper } from "react-icons/im";
-import { GiDiamondTrophy, GiEclipse } from "react-icons/gi";
+import { GiDiamondTrophy, GiHanger } from "react-icons/gi";
 import { RiLightbulbLine, RiFeedbackLine } from "react-icons/ri";
 import { FiSettings, FiHelpCircle } from "react-icons/fi";
 import { IoLogoJavascript } from "react-icons/io";
 import { FaReact } from "react-icons/fa";
+import { SiYoutubegaming } from "react-icons/si";
 
 export const categories = [
   { name: "New", icon: <AiFillHome />, type: "home" },
@@ -20,13 +20,13 @@ export const categories = [
   { name: "Music", icon: <CgMusicNote />, type: "category" },
   { name: "Films", icon: <FiFilm />, type: "category" },
   { name: "Live", icon: <MdLiveTv />, type: "category" },
-  { name: "Gaming", icon: <IoGameControllerSharp />, type: "category" },
+  { name: "Gaming", icon: <SiYoutubegaming />, type: "category" },
   { name: "News", icon: <ImNewspaper />, type: "category" },
   { name: "Sports", icon: <GiDiamondTrophy />, type: "category" },
   { name: "Learning", icon: <RiLightbulbLine />, type: "category" },
   {
     name: "Fashion & beauty",
-    icon: <GiEclipse />,
+    icon: <GiHanger />,
     type: "category",
     divider: true,
   },
